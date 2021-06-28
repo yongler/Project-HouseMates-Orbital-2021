98a3a1ac2ef70fed7d452bf7dab8c2eb9e940dc1
yongler <leeyongler1.618@gmail.com>
2021-06-28 23:26:55 +0800

    milestone 2 heroku

--- HouseMates/urls.py
+++ HouseMates/urls.py
@@ -17,5 +17,7 @@ urlpatterns = [
     # url(r'^graphql', csrf_exempt(TemplateView.as_view(graphiql=True))),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += [re_path(r"^.*", TemplateView.as_view(template_name="index.html"))]
