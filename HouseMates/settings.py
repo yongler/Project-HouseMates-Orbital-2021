50da9e55c4330791db49359018b7df7f8e32aaa1
yongler <leeyongler1.618@gmail.com>
2021-07-24 22:23:29 +0800

    heroku

--- HouseMates/settings.py
+++ HouseMates/settings.py
@@ -181,7 +181,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static')
