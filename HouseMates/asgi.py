1b7074905fcc80a2c77632a5c12eb4100b024888
yongler <leeyongler1.618@gmail.com>
2021-07-24 10:47:43 +0800

    heroku

--- HouseMates/asgi.py
+++ HouseMates/asgi.py
@@ -1,23 +1,3 @@
-"""
-ASGI config for HouseMates project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-# For more information on this file, see
-# https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-# """
-
-# import os
-
-# from django.core.asgi import get_asgi_application
-
-# import django
-# from channels.routing import get_default_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HouseMates.settings')
-# django.setup()
-# application = get_default_application()
-
 import os
 
 from channels.auth import AuthMiddlewareStack
--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 release: python manage.py migrate
-web: gunicorn HouseMates.wsgi --log-file -
+web: daphne HouseMates.asgi:application --port $PORT --bind 0.0.0.0
+worker: python manage.py runworker channel_layer
