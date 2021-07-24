3a21bc9f285fd171b4099e4f431e726420059968
yongler <leeyongler1.618@gmail.com>
2021-07-24 21:38:32 +0800

    heroku

--- src/components/ChatListItem.js
+++ src/components/ChatListItem.js
@@ -1,73 +1,138 @@
-import React, { useState } from "react"
-import clsx from 'clsx'
-import { Avatar, ButtonBase, ListItem, ListItemAvatar, ListItemText, Paper, makeStyles, Typography } from '@material-ui/core'
+import React, { useState } from "react";
+import clsx from "clsx";
+import {
+  Avatar,
+  ButtonBase,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Paper,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
 
-const ChatListItem = ({ user, name, pic, msg, time, unreadMsgs, animationDelay, active, setRoom, room, editMsg }) => {
+const ChatListItem = ({
+  user,
+  name,
+  pic,
+  msg,
+  time,
+  unreadMsgs,
+  animationDelay,
+  active,
+  setRoom,
+  room,
+  editMsg,
+}) => {
   // Styling
-  const useStyles = makeStyles(theme => ({
+  const useStyles = makeStyles((theme) => ({
     active: {
       backgroundColor: theme.palette.action.hover,
     },
-  }))
+  }));
 
   // Hooks
-  const classes = useStyles()
+  const classes = useStyles();
 
   // States
-  const [enter, setEnter] = useState(false)
+  const [enter, setEnter] = useState(false);
 
   // Handlers
   const handleClick = () => {
-    setRoom(room.label)
-    room.messages.forEach(msg => {
-      if (!msg.hasRead && msg.user_id.toString() !== user.id.toString()) editMsg(msg.id, true)
-    })
-  }
-  const handleMouseEnter = () => { setEnter(true) }
-  const handleMouseLeave = () => { setEnter(false) }
+    setRoom(room.label);
+    room.messages.forEach((msg) => {
+      if (!msg.hasRead && msg.user_id.toString() !== user.id.toString())
+        editMsg(msg.id, true);
+    });
+  };
+  const handleMouseEnter = () => {
+    setEnter(true);
+  };
+  const handleMouseLeave = () => {
+    setEnter(false);
+  };
 
   return (
     <Paper style={{ width: "100%", marginBottom: 5, minWidth: 250 }}>
       <ButtonBase
         style={{ width: "100%", borderRadius: 5 }}
-        className={clsx({ [classes.active]: (active || enter) })}
+        className={clsx({ [classes.active]: active || enter })}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       >
         <ListItem
-          style={{ animationDelay: `0.${animationDelay}s`, marginBottom: 10, display: "flex" }}
+          style={{
+            animationDelay: `0.${animationDelay}s`,
+            marginBottom: 10,
+            display: "flex",
+          }}
           onClick={handleClick}
         >
           <ListItemAvatar>
             <Avatar src={pic} />
           </ListItemAvatar>
-          <div style={{ display: 'flex', flexDirection: "column", width: "100%", overflow: "hidden" }}>
-            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "100%",
+              overflow: "hidden",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
               {/* Name */}
               <Typography variant="body1">{name}</Typography>
 
               {/* Unread messages */}
-              {unreadMsgs !== 0 &&
-                <div style={{ width: 20, height: 20, backgroundColor: "red", display: "flex", justifyContent: "center", alignItems: "center", borderRadius: "50%" }}>
-                  <div style={{ color: "white", textAlign: "center" }}>{unreadMsgs}</div>
-                </div>}
+              {unreadMsgs !== 0 && (
+                <div
+                  style={{
+                    width: 20,
+                    height: 20,
+                    backgroundColor: "red",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderRadius: "50%",
+                  }}
+                >
+                  <div style={{ color: "white", textAlign: "center" }}>
+                    {unreadMsgs}
+                  </div>
+                </div>
+              )}
             </div>
             <div style={{ display: "flex", justifyContent: "space-between" }}>
               {/* Latest message */}
               <Typography variant="body2" color="textSecondary">
-                {msg.length > 15 ? msg.substring(0, 15) + "..." : msg}
+                {msg && (msg.length > 15 ? msg.substring(0, 15) + "..." : msg)}
               </Typography>
 
               {/* Time */}
-              <Typography variant="body2" color="textSecondary" style={{ overflow: "hidden" }}>
-                {time.split(" ")[1].split(":")[0] + ":" + time.split(" ")[1].split(":")[1] + " " + time.split(" ")[2]}
+              <Typography
+                variant="body2"
+                color="textSecondary"
+                style={{ overflow: "hidden" }}
+              >
+                {time &&
+                  time.split(" ")[1].split(":")[0] +
+                    ":" +
+                    time.split(" ")[1].split(":")[1] +
+                    " " +
+                    time.split(" ")[2]}
               </Typography>
             </div>
           </div>
         </ListItem>
       </ButtonBase>
     </Paper>
-  )
-}
+  );
+};
 
-export default ChatListItem
+export default ChatListItem;
--- src/components/ProfileComponent.js
+++ src/components/ProfileComponent.js
@@ -1,58 +1,94 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { Card, CardActionArea, CardContent, CardMedia, Chip, Typography } from '@material-ui/core'
-import { useHistory, Link } from 'react-router-dom'
-import { ROOMMATE_FORM } from '../globalConstants'
-import { setChatUser } from '../redux/chat/actions'
+import React from "react";
+import { connect } from "react-redux";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Chip,
+  Typography,
+} from "@material-ui/core";
+import { useHistory, Link } from "react-router-dom";
+import { ROOMMATE_FORM } from "../globalConstants";
+import { setChatUser } from "../redux/chat/actions";
 
-const ProfileComponent = ({ name, desc, pic, scoreList, unreadMsgs, type, id, chatUser, setChatUser }) => {
+const ProfileComponent = ({
+  name,
+  desc,
+  pic,
+  scoreList,
+  unreadMsgs,
+  type,
+  id,
+  chatUser,
+  setChatUser,
+}) => {
   // Hooks
-  const history = useHistory()
+  const history = useHistory();
 
   // Handlers
   const handleClick = () => {
     if (type === ROOMMATE_FORM) {
-      history.push(`/roommates/${id}`)
+      history.push(`/roommates/${id}`);
     } else if (type === ROOMMATE_FORM) {
-      history.push(`/housings/${id}`)
+      history.push(`/housings/${id}`);
     } else {
-      setChatUser(chatUser)
-      history.push('/chat')
+      setChatUser(chatUser);
+      history.push("/chat");
     }
-  }
+  };
   return (
-    <Link onClick={handleClick} style={{ textDecoration: 'none' }}>
+    <Link onClick={handleClick} style={{ textDecoration: "none" }}>
       <Card style={{ display: "flex", flexDirection: "row", margin: 10 }}>
-        <CardMedia
-          image={pic}
-          title={name}
-          style={{ width: 100 }}
-        />
+        <CardMedia image={pic} title={name} style={{ width: 100 }} />
         <CardActionArea>
           <CardContent>
-            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", width: "100%" }}>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                width: "100%",
+              }}
+            >
               <Typography>{name}</Typography>
-              {type === ROOMMATE_FORM
-                ? <Chip label={scoreList[id]?.score + "%"} color="secondary" size="small" />
-                : !type
-                  ?
-                  <>
-                    {unreadMsgs !== 0 &&
-                      <div style={{ width: 20, height: 20, backgroundColor: "red", display: "flex", justifyContent: "center", alignItems: "center", borderRadius: "50%" }}>
-                        <div style={{ color: "white", textAlign: "center" }}>{unreadMsgs}</div>
-                      </div>}
-                  </>
-                  : null}
+              {type === ROOMMATE_FORM ? (
+                <Chip
+                  label={scoreList[id]?.score + "%"}
+                  color="secondary"
+                  size="small"
+                />
+              ) : !type ? (
+                <>
+                  {unreadMsgs !== 0 && (
+                    <div
+                      style={{
+                        width: 20,
+                        height: 20,
+                        backgroundColor: "red",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        borderRadius: "50%",
+                      }}
+                    >
+                      <div style={{ color: "white", textAlign: "center" }}>
+                        {unreadMsgs}
+                      </div>
+                    </div>
+                  )}
+                </>
+              ) : null}
             </div>
             <Typography variant="body1" color="textSecondary">
-              {desc.length > 25 ? desc.substring(0, 25) + "..." : desc}
+              {desc && (desc.length > 25 ? desc.substring(0, 25) + "..." : desc)}
             </Typography>
           </CardContent>
         </CardActionArea>
       </Card>
     </Link>
-  )
-}
+  );
+};
 
 const mapDispatchToProps = {
   setChatUser,
