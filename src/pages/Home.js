bfa3f8ac82345e24b1eeff5328d655e532c50cb3
yongler <leeyongler1.618@gmail.com>
2021-08-02 14:05:20 +0800

    last heroku

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -155,22 +155,6 @@ const Home = ({
                 frameborder="0"
                 allowfullscreen
               ></iframe>
-              {/* <iframe
-                id="existing-iframe-example"
-                width="640"
-                height="360"
-                src="https://youtu.be/jtneFF4psgM?autoplay=1&mute=1&enablejsapi=1"
-                frameborder="0"
-                allow="autoplay"
-                style={{ border: "solid 4px #37474F" }}
-              ></iframe> */}
-              {/* <iframe
-                type="text/html"
-                src="https://youtu.be/jtneFF4psgM?autoplay=1"
-                frameborder="0"
-                allow="autoplay"
-                X-Frame-Options="cross-origin"
-              ></iframe> */}
             </Grid>
 
             {/* Login & register */}
